ae56560a0491d83ae8700f4819c7ca840fd423a4
Harash <harash.b13@gmail.com>
2024-01-04 15:52:25 -0500

    Initial Commit

---
+++ src/main/java/client/chatapp_client/App_Controller.java
@@ -0,0 +1,37 @@
+package client.chatapp_client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
+public class App_Controller {
+
+    @FXML
+    protected AnchorPane anchor_conv;
+
+    @FXML
+    protected AnchorPane anchor_user;
+
+    @FXML
+    protected Button btSend;
+
+    @FXML
+    protected Label conversation;
+
+    @FXML
+    protected GridPane grid;
+
+    @FXML
+    protected Label lb_enter;
+
+    @FXML
+    protected TextArea taServer = new TextArea();
+
+    @FXML
+    protected TextField tfClient = new TextField();
+
+}
